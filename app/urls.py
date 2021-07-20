69712db489c5ccae17800289e0457ae2a0d7b61b
joacoh <jhyevenes@outlook.es>
2021-07-19 22:11:21 -0400

    Delete all the things

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, contacto, ramos
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('contacto/', contacto, name='contacto'),
-    path('ramos/', ramos, name='ramos')
-]
